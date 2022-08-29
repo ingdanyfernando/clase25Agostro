3f2aa65cd3dc72964f1206431d8e16d8c900bf32
ingdanyfernando <ing.danyfernando@gmail.com>
2022-08-29 17:58:27 -0600

    Modelo Estudiantes Construccion

---
+++ EstudianteModel.php
@@ -0,0 +1,21 @@
+<?php
+class Estudiante
+{
+    private $apellidos;
+    private $nombre;
+    private $direccion;
+    private $telefono;
+
+    public function __construct($ape,$nom,$dir,$tel)
+    {
+        $this->apellidos = $ape;
+        $this->nombre = $nom;
+        $this->direccion = $dir;
+        $this->telefono = $tel;
+    }
+
+    public function GuardarEstudiante($ape,$nom,$dir,$tel)
+    {
+       /*CONEXION A LA BASE DE DATOS*/ 
+    }
+}
