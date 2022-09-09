4d9eb741fca4144cfc2394921945c156c1ac6669
ingdanyfernando <ing.danyfernando@gmail.com>
2022-09-08 18:16:46 -0600

    Formulario EditarEstudiante

--- VistaEditarEstudiante.php
+++ VistaEditarEstudiante.php
@@ -12,7 +12,40 @@
                             include_once "EstudianteModel.php";
                             $nuevoEstudiante = new Estudiante();
                             /*FILTRAR AL ESTUDIANTE SEGUN ID ENVIADO*/
+                            $resultado = $nuevoEstudiante->FiltrarEstudiante($_GET['idEst']);
+
+                            while($resultadoFiltrado = mysqli_fetch_assoc([$resultado])
+                            {
                         ?>
+                                <p>
+                                <label for="txtapellidos">Apellidos:</label>
+                                <input type="text" name="txtapellidos" 
+                                value="<?php echo $resultadoFiltrado['apellidos']?>">
+                                </p>
+
+                                <p>
+                                <label for="txtnombre">Nombre:</label>
+                                <input type="text" name="txtnombre" 
+                                value="<?php echo $resultadoFiltrado['nombre']?>">
+                                </p>
+
+                                <p>
+                                <label for="txtdireccion">Direccion:</label>
+                                <input type="text" name="txtdireccion" 
+                                value="<?php echo $resultadoFiltrado['direccion']?>">
+                                </p>
+
+                                <p>
+                                <label for="txttelefono">Telefono:</label>
+                                <input type="text" name="txttelefono" 
+                                value="<?php echo $resultadoFiltrado['telefono']?>">
+                                </p>
+
+                                
+                            <?php
+                            }
+                            ?>
+                        
 
     </form>
 </body>
